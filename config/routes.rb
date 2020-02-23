9ee2b6f9e0c2f41ff48749ecaaebbed6b5076bca
Vera Kalwa <vera.kalwa@gmail.com>
2020-02-23 16:00:24 +0100

    root path<2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:show, :new, :create]
   end
     resources :doses, only: [:destroy]
+    root to: 'cocktails#index'
+    resources :ocktails, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
